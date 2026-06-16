8b76bb62cf00f13b425861f87fc780e2ff75e39d
Manuel Fernandes <mfmanu24@gmail.com>
2026-06-16 14:49:06 -0300

    fix: resolve api data generation logging

--- app/backend/src/controllers/dataController.js
+++ app/backend/src/controllers/dataController.js
@@ -1,18 +1,34 @@
-const service = require("../services/dataService");
+const logger = require("../logging/logger");
+const { faker } = require("@faker-js/faker");
 
 
 exports.generate = (req,res)=>{
 
 
-    const data = service.create();
+logger.info("Generating fake data");
 
 
-    req.log.info({
-        event:"data_generated",
-        payload:data
-    });
+const data = {
+
+ id: faker.string.uuid(),
+
+ user: faker.person.fullName(),
+
+ email: faker.internet.email(),
+
+ amount:
+ faker.number.int({
+ min:100,
+ max:1000
+ }),
+
+ created:
+ new Date()
+
+};
 
 
-    res.json(data);
+res.json(data);
+
 
 };
--- app/backend/src/server.js
+++ app/backend/src/server.js
@@ -1,44 +1,30 @@
 const express = require("express");
-const pinoHttp = require("pino-http");
-
-const logger = require("./logging/logger");
-
-const apiRoutes = require("./routes/api");
-
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json());
-
 app.use(
-    pinoHttp({
-        logger
+    cors({
+        origin: "http://localhost:5173"
     })
 );
 
-
-app.use("/api", apiRoutes);
+app.use(express.json());
 
 
 app.get("/health", (req,res)=>{
-
-    req.log.info("health check executed");
-
     res.json({
         status:"UP",
         service:"lgtm-api"
     });
-
 });
 
 
-const PORT = process.env.PORT || 3000;
+app.use("/api", require("./routes/api"));
 
 
-app.listen(PORT,()=>{
+const PORT = 3000;
 
-    logger.info(
-        `Application running on port ${PORT}`
-    );
-
+app.listen(PORT, ()=>{
+    console.log(`API running on port ${PORT}`);
 });
