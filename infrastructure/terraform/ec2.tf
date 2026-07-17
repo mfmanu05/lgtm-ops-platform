e648b410a87640ea510bced7256426df6ec48d85
Manuel Fernandes Andrade <mfmanu24@gmail.com>
2026-07-17 15:12:11 -0300

    update terraform ec2

--- infrastructure/terraform/ec2.tf
+++ infrastructure/terraform/ec2.tf
@@ -1,27 +1,23 @@
 resource "aws_instance" "lgtm" {
-
-
-  ami = data.aws_ami.ubuntu.id
-
-
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-
-
-  key_name = var.key_name
-
+  key_name      = var.key_name
 
   vpc_security_group_ids = [
     aws_security_group.lgtm_sg.id
   ]
 
-
   user_data = file("user_data.sh")
 
-
-  tags = {
-
-    Name = var.project_name
-
+  # 👉 Configuração do volume raiz adicionada aqui:
+  root_block_device {
+    volume_size           = 50    # Tamanho em Gigabytes (mude se precisar de mais)
+    volume_type           = "gp3" # Tipo de volume recomendado pela AWS
+    delete_on_termination = true  # Apaga o disco automaticamente se a instância for destruída
+    encrypted             = true  # Opcional: Garante que os dados no disco fiquem criptografados
   }
 
+  tags = {
+    Name = var.project_name
+  }
 }
